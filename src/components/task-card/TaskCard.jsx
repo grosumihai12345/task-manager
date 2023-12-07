946a142f6a86bc267b8f7b7bdfb0c3dd710c20c1
grosumihai1234 <grosumihai1234@gmail.com>
2023-12-07 17:27:45 +0200

    Added components: CreateTaskForm

--- src/components/task-card/TaskCard.jsx
+++ src/components/task-card/TaskCard.jsx
@@ -1,26 +1,19 @@
+import React from "react";
 import "./TaskCard.css";
 import Badge from "../badge/Badge";
 import DateContainer from "../date-container/DateContainer";
 
 function TaskCard(props) {
-  let taskId = props.id;
-
-  const handleClick = () => {
-    console.log("Clicked! " + props.id);
-    taskId = taskId + "Clicked !";
-  };
-
   return (
     <div className="card-wrapper">
       <div className="card-header">
-        <p className="task-id">{taskId}</p>
+        <p className="task-id">{props.id}</p>
         <Badge status={props.status} />
       </div>
       <div className="card-content">
         <p>{props.name}</p>
       </div>
       <div className="card-footer">
-        <button onClick={handleClick}>Click me!</button>
         <DateContainer date={props.dueDate} />
       </div>
     </div>
